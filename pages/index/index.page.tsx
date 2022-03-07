b75ad5c74d514aeb87ef08f1dd9b51366c3e5a04
James Pieper <James@jamespieper.com>
2022-03-07 12:20:10 -0600

    add navigate version

--- pages/index/index.page.tsx
+++ pages/index/index.page.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import MobileNavigation from '../../components/mobilenav'
+import MobileNavigationNavigate from '../../components/Mobilenav-navigate'
 import MobileNavigationNoHeadless from '../../components/mobilenav-noheadlessui'
 import MobileNavigationNoHeadless2 from '../../components/mobilenav-noheadlessui-2'
+
 import { Counter } from './Counter'
 
 export { Page }
@@ -10,11 +12,13 @@ function Page() {
   const [open1, setOpen1] = useState(false)
   const [open2, setOpen2] = useState(false)
   const [open3, setOpen3] = useState(false)
+  const [open4, setOpen4] = useState(false)
   return (
     <>
     <MobileNavigation open={open1} setOpen={setOpen1} />
     <MobileNavigationNoHeadless open={open2} setOpen={setOpen2} />
     <MobileNavigationNoHeadless2 open={open3} setOpen={setOpen3} />
+    <MobileNavigationNavigate open={open4} setOpen={setOpen4} />
       <h1>Welcome</h1>
       This page is:
       <ul>
@@ -22,6 +26,7 @@ function Page() {
         <button onClick={() => setOpen1(true)}>Open Nav</button>
         <button onClick={() => setOpen2(true)}>Open Nav No headless - Hidden With CSS</button>
         <button onClick={() => setOpen3(true)}>Open Nav No headless - Removed From DOM</button>
+        <button onClick={() => setOpen4(true)}>Open Nav navigate vs {`<a>tag</a>`}</button>
         <li>
           Interactive. <Counter />
         </li>
